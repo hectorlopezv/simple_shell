37ac89bc9a0648d49757951cbb54a62bcf5b1771
Samuel <samgj_18@hotmail.com>
2020-04-17 16:27:26 -0500

    Dividing function into more specific files

--- utils.c
+++ utils.c
@@ -79,27 +79,3 @@ int _strncmp(char *s1, const char *s2, int size)
 	return (0);
 }
 
-/**
- * _getenv - looks for the path
- * @name: variable to be look
- * Return: a pointer to a string
- */
-char *_getenv(const char *name)
-{
-	char **env;
-	size_t  i;
-	char *p;
-
-	env = environ;
-	for (i = 0; env[i] != NULL; i++)
-	{
-		if (strncmp(env[i], name, 4) == 0)
-		{
-			p = env[i];
-			return (p);
-		}
-
-	}
-	return (NULL);
-}
-
