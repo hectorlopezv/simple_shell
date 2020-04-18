513dd27ca8ef11b0a75f7f2833e5b86982c43327
hector vinicio lopez <hectorvmlopez@gmail.com>
2020-04-18 19:13:20 +0000

    changes to strdup

--- more_utils.c
+++ more_utils.c
@@ -60,22 +60,36 @@ char *_convert(long int num, int base)
  **/
 char *_strdup(char *str)
 {
-	int size, i;
-	char *dest;
+	int i, j;
+	char *C;
 
 	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+
+	}
+
+	C = (char *) malloc(j + 1);
+
+	if (C == NULL)
+	{
 		return (NULL);
 
-	size = _strlen(str);
-	dest = malloc(size * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
+	}
 
-	for (i = 0; i < size; i++)
-		*(dest + i) = *(str + i);
+	for (i = 0; i < j; i++)
+	{
+		C[i] = str[i];
+	}
 
-	/* (dest + i) = 0; */
+	C[i] = str[i];
 
-	return (dest);
+	return (C);
 }
+
 
